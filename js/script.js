7558f2fdae3d00e148fd07e3bc9c3608065c8572
julliasenac8-debug <julliasenac8@gmail.com>
2026-05-15 11:27:10 -0300

    alteração da logo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function enviar(){
-    window.confirm("Deseja enviar seu formulário?")
-    window.alert("Seu formulário foi enviado com sucesso.")
-
+    if (window.confirm("Deseja enviar seu formulário?")) {
+        window.alert("Seu formulário foi enviado com sucesso.");
+    }
 }
